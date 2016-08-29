3ffec11801cea707e34bc8ccbf711d410df5892b
Dale Ma <dalema22@gmail.com>
2016-08-29 00:24:43 -0700

    refactor

--- addon/mixins/markdown-editor.js
+++ addon/mixins/markdown-editor.js
@@ -14,66 +14,83 @@ class Strategy {
   }
 
   _createStrategy(tagName, editor) {
+    let token = '';
+    function _run(editor, matches, tagName, symbolLength) {
+        let [, , matched] = matches;
+        let { range } = editor;
+        range = range.extend(-1 * (matched.length + symbolLength));
+
+        editor.run(postEditor => {
+          let position = postEditor.deleteRange(range);
+          let markup = postEditor.builder.createMarkup(tagName);
+          let nextPosition = postEditor.insertTextWithMarkup(position, matched, [markup]);
+          requestAnimationFrame(() => { editor.toggleMarkup(tagName) });
+        });
+    }
+
+    function _willDeleteCallback(range, direction, tagName) {
+      editor.run(postEditor => {
+        let [markups, text] = extractMarkupsAndText(range);
+
+        if (markups.length === 1) {
+          let [markup] = markups;
+
+          if (direction === -1 && range.isCollapsed && markup.tagName === tagName) {
+            token = text;
+          }
+        }
+      });
+    }
+
+    function _didDeleteCallback(range, markdownSyntax) {
+      editor.run(postEditor => {
+        let [markups] = extractMarkupsAndText(range);
+
+        if (token) {
+          let [markup] = markups;
+          // deleted one unit, so move forward one unit and shink the range by 1 unit
+          let extendLength = token.length > 1 ? token.length - 1 : 0;
+          range = range.move(-1).extend(-1 * extendLength);
+          let position = postEditor.deleteRange(range);
+          postEditor.setRange(position);
+          postEditor.insertTextWithMarkup(position, markdownSyntax(token), []);
+          // reset token
+          token = '';
+        }
+      });
+    }
+
     const strategies = {
       em() {
-        let token = '';
-
+        const tagName = 'em';
+        const markdownSyntax = (t) => `*${t}*`;
         return {
           onTextInputCallback: {
             match: /(\*|_)(\S+)\1 /,
-            run(editor, matches) {
-              let [, , matched] = matches;
-              let { range } = editor;
-              // extend the range backward to cover the text + the 2 "*" + 1 " "
-              range = range.extend(-1 * (matched.length + 2 + 1));
-
-              editor.run(postEditor => {
-                const marker = 'em';
-                let position = postEditor.deleteRange(range);
-                let markup = postEditor.builder.createMarkup(marker);
-                let nextPosition = postEditor.insertTextWithMarkup(position, matched, [markup]);
-                requestAnimationFrame(() => { editor.toggleMarkup(marker) });
-              });
-            }
+            // extend the range backward to cover the text + the 2 "*" + 1 " "
+            run: (editor, matches) => _run(editor, matches, tagName, 2 + 1)
           },
-          willDeleteCallback(range, direction, unit) {
-            let { builder, cursor } = editor;
+          willDeleteCallback: (range, direction) => _willDeleteCallback(range, direction, tagName),
+          didDeleteCallback: (range) => _didDeleteCallback(range, markdownSyntax)
+        };
+      },
 
-            editor.run(postEditor => {
-              let [markups, text] = extractMarkupsAndText(range);
-
-              if (markups.length === 1) {
-                let [markup] = markups;
-
-                if (direction === -1 && range.isCollapsed && markup.tagName === 'em') {
-                  range = range.extend(-1 * (text.length));
-                  token = text;
-                }
-              }
-            });
+      strong() {
+        const tagName = 'strong';
+        const markdownSyntax = (t) => `**${token}**`;
+        return {
+          onTextInputCallback: {
+            match: /(\*\*|__)(\S+)\1 /,
+            // extend the range backward to cover the text + the 4 "*" + 1 " "
+            run: (editor, matches) => _run(editor, matches, tagName, 4 + 1)
           },
-          didDeleteCallback(range, direction, unit) {
-            editor.run(postEditor => {
-              let [markups] = extractMarkupsAndText(range);
-
-              if (token) {
-                let [markup] = markups;
-                // deleted one unit, so move forward one unit and shink the range by 1 unit
-                let extendLength = token.length > 1 ? token.length - 1 : 0;
-                range = range.move(-1).extend(-1 * extendLength);
-                let position = postEditor.deleteRange(range);
-                postEditor.setRange(position);
-                postEditor.insertTextWithMarkup(position, `*${token}*`, []);
-                // reset token
-                token = '';
-              }
-            });
-          }
+          willDeleteCallback: (range, direction) => _willDeleteCallback(range, direction, tagName),
+          didDeleteCallback: (range) => _didDeleteCallback(range, markdownSyntax)
         };
       },
 
       error() {
-        throw new Exception(`Unable to find ${tagName} strategy`);
+        throw new Error(`Unable to find ${tagName} strategy`);
       }
     };
 
@@ -85,12 +102,19 @@ class Strategy {
 export default Ember.Mixin.create({
   actions: {
     setup(editor) {
-      const { callbacks: {
-        onTextInputCallback, willDeleteCallback, didDeleteCallback
-      } } = new Strategy('em', editor);
-      editor.onTextInput(onTextInputCallback);
-      editor.willDelete(willDeleteCallback);
-      editor.didDelete(didDeleteCallback);
+      let tagNames = ['strong', 'em', 'h1'];
+
+      tagNames
+        .map(tag => (new Strategy(tag, editor)) )
+        .map(strategy => strategy.callbacks)
+        .forEach(callbacks => {
+          const {
+            onTextInputCallback, willDeleteCallback, didDeleteCallback
+          } = callbacks;
+          editor.onTextInput(onTextInputCallback);
+          editor.willDelete(willDeleteCallback);
+          editor.didDelete(didDeleteCallback);
+        });
     }
   }
 });
